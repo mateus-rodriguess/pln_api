768ef6b7d532496b59fb732522c7f96441836564
Mateus Rodrigues <mateus18nc@gmail.com>
2022-06-07 20:25:31 -0300

    add conf de token JWT

--- apps/config.py
+++ apps/config.py
@@ -4,17 +4,21 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_name: str = "API PLN"
-    vocab_fname: str = "apps/pln/services/ttVocab"
-    path_my_weights: str = 'apps/pln/weights_folder/my_weights'
+    APP_NAME: str = "API PLN"
+
+    VOCAB_FNAME: str = "apps/pln/services/ttVocab"
+    PATH_MY_WEIGHTS: str = 'apps/pln/weights_folder/my_weights'
     SQLALCHEMY_DATABASE_URL: str = "postgresql://admin:admin@db/app"
     TRAIN_CSV:  str = 'apps/pln/data/trainingandtestdata/train.csv'
     TEST_CSV: str = "apps/pln/data/trainingandtestdata/test.csv"
     
+    SECRET_KEY: str = "hfaushbfub4u23b4u32b4b324j3" # default
+    ALGORITHM: str = "HS256" # default
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 80
+
     class Config:
         case_sensitive = True
         env_prefix = ''
-        # não estou ultilizando .env
         env_file = '../.env'
         env_file_encoding = 'utf-8'
 
