108d6842ae89ff14217194b63ff97c73be9ac8db
Mateus Rodrigues <mateus18nc@gmail.com>
2022-06-13 16:33:32 -0300

    feat: add save translate

--- apps/crud/accuracy_crud.py
+++ apps/crud/accuracy_crud.py
@@ -2,22 +2,34 @@ from email import message
 from sqlalchemy.orm import Session
 from apps.models.accuracy_models import AccuracyModel
 from apps.schemas import accuracy_schema
-from apps.pln.accuracy import accuracy_response
+from apps.schemas.accuracy_schema import AccuracyResponse
+from apps.pln.accuracy import accuracy_predict
 from apps.services.translate_message import translate
 
-def add_accuracy(db: Session, accuracy_data: accuracy_schema.AccuracyResponse):
+def add_accuracy(db: Session, accuracy_data: accuracy_schema.Accuracy):
     """
     Save accuracy
     """
-    message = translate(accuracy_data.message)
-    accuracy =  accuracy_response(text=message)
     
-    db_accuracy = AccuracyModel(accuracy=accuracy, message=message)
-    
-    db.add(db_accuracy)
-    db.commit()
-    db.refresh(db_accuracy)
-    return db_accuracy
+    if accuracy_data.translate:
+        message = translate(accuracy_data.message)
+        accuracy =  accuracy_predict(text=message)
+    else:
+        message = accuracy_data.message
+        accuracy =  accuracy_predict(text=message)
+
+    if not accuracy_data.save:
+        AccuracyResponse.message = message
+        AccuracyResponse.accuracy = accuracy
+        
+        return AccuracyResponse
+    else:
+        db_accuracy = AccuracyModel(accuracy=accuracy, message=message)
+        db.add(db_accuracy)
+        db.commit()
+        db.refresh(db_accuracy)
+        return db_accuracy
+
 
 def list_accuracy(db: Session):
     return db.query(AccuracyModel).filter().all()
