6308cc064eda49a2e0567f7c3edc35fb4272a0ef
mateus <mateus-rodriguess>
2022-07-12 13:40:52 -0300

    change: db postgres

--- test/test_sql_app.py
+++ test/test_sql_app.py
@@ -1,15 +1,14 @@
-from app.database import Base, get_db
-from app.main import app
+from apps.database import Base, get_db
+from apps.main import app
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 # db url test
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@test-postgres/postgres"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
-                       "check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
 
