ef11e60ff4d63adc2260dfd26f27c53128a9903f
mateus <mateus-rodriguess>
2022-07-13 13:34:11 -0300

    feat: try exception if the weights do not exist

--- apps/pln/accuracy.py
+++ apps/pln/accuracy.py
@@ -1,30 +1,35 @@
 import logging
 import os
-
-import numpy as np
-
+# ok
 logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 logging.getLogger('tensorflow').disabled = True
 
-import tensorflow_datasets as tfds
-from apps.config import get_settings
+
 from apps.pln.dm.dcnn import DCNN
+from apps.config import get_settings
+import tensorflow_datasets as tfds
+import numpy as np
+
 
 settings = get_settings()
 
 Dcnn = DCNN()
 Dcnn.built = True
 
-Dcnn.load_weights(settings.PATH_MY_WEIGHTS).expect_partial()
-encoder = tfds.deprecated.text.SubwordTextEncoder.load_from_file(settings.VOCAB_FNAME)
-
 
 def accuracy_predict(text: str):
     """
     Accuracy predict
     """
-    text = encoder.encode(text)
-    [[accuracy]] =  Dcnn.predict(np.array([text]))
-   
-    return accuracy.astype(float)
+    try:
+        # if the error of the file does not exist it returns float 0.0
+        Dcnn.load_weights(settings.PATH_MY_WEIGHTS).expect_partial()
+        encoder = tfds.deprecated.text.SubwordTextEncoder.load_from_file(
+            settings.VOCAB_FNAME)
+        text = encoder.encode(text)
+        # feedback prediction
+        [[accuracy]] = Dcnn.predict(np.array([text]))
+        return accuracy.astype(float)
+    except Exception as erro:
+        return 0.0
